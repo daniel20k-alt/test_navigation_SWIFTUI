cd8da9e4f2dc4ec88f48808373ee6527da84a8cb
Testing_author <testing_beta@mail.com>
2020-12-14 23:59:28 +0200

    Adjusting the view of the app.

--- Stacks/ContentView.swift
+++ Stacks/ContentView.swift
@@ -43,6 +43,7 @@ import SwiftUI
 
 struct ContentView: View {
   let mix = MeowMix()
+    @State private var showMessage = false
   
   var body: some View {
 // MARK: TODO - Add a NavigationView
@@ -50,25 +51,37 @@ struct ContentView: View {
      * Try using the .navigationViewStyle modifier on the NavigationView with a StackNavigationViewStyle()
      * Try hiding the navigation bar with .navigationBarHidden
 */
+    NavigationView {
     VStack(spacing: 0.0) {
       MeowMixHeader()
         .padding()
         .onTapGesture {
-          // trigger message popover here
+  showMessage = true
         }
+        
+        .popover(isPresented: $showMessage, content: {
+                MessagePopover()
+        })
       
       Divider()
         .padding()
       
       List(mix.tracks) { track in
-        TrackRow(track: track)
+        NavigationLink(
+            destination: DetailView(track: track),
+            label: {
+                TrackRow(track: track)
+            })
       }
       
       FeaturedCats(artists: mix.tracks.map(\.artist))
         .padding(.vertical)
         .background(Color.gray.opacity(0.2))
     }
+    .navigationBarHidden(true)
   }
+    .navigationViewStyle(StackNavigationViewStyle())
+}
 }
 
 struct DetailView: View {
